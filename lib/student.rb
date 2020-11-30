22b3f21e808f74187c1d95d2b73d0bee8814a215
Olivia Hanson <olivia.joy.hanson@gmail.com>
2020-11-30 22:36:49 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,7 @@
 class Student
   attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
+
+  def initialize(hash)
+    hash.each{|key, value| self.send("#{key}=", value)}
+  end 
 end
